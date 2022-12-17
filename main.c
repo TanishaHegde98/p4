5f17f470dd69c4f8df368f1caceb9d92019b6fdb
SJUWM <117421227+SJUWM@users.noreply.github.com>
2022-12-17 17:09:48 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include "mfs.h"
 
 int main(int argc, char *argv[]) {
-    int rc = MFS_Init("localhost", 10000);
+    int port=atoi(argv[1]);
+    int rc = MFS_Init("localhost", port);
     printf("init %d\n", rc);
 
     //printf("lookup %d\n", rc);
 
     //rc = MFS_Creat(0,0,"_newdir");
     //rc = MFS_Creat(1,0,"new_in_newdir");
-    rc = MFS_Creat(1,0,"testDir");
+    rc = MFS_Creat(0,0,"testDir");
+    printf("\nRC:%d",rc);
+    rc = MFS_Creat(1,0,"/testDir/test.txt");
+    printf("\nRC:%d",rc);
     //rc = MFS_Lookup(1,".");
     //rc = MFS_Lookup(1,"new_in_newdir");
-    rc = MFS_Lookup(1,"test.txt");
+    rc = MFS_Lookup(0,"testDir");
+    printf("\nRC:%d",rc);
     rc = MFS_Shutdown();
+    printf("\nRC:%d",rc);
+
     printf("shut %d\n", rc);
     return 0;
 }
